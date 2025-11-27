8251ea3fff4589a32124f6ae6afd860bb525b194
deppy0 <dependant1b@gmail.com>
2025-11-27 20:47:33 +0800

    Restored training_error for benchmarking

--- catbot/training.py
+++ catbot/training.py
@@ -136,7 +136,7 @@ def train_bot(cat_name, render: int = -1):
     
     #naka define na pala yung env sa function bruh
     #episodes already defined
-    #training_error = []
+    training_error = []
     last_num_moves = 0
     start_time = time.perf_counter()
     
@@ -171,7 +171,7 @@ def train_bot(cat_name, render: int = -1):
             reward = getReward(next_state, moves)
             #step 5
             q_table, temporal_difference = update(q_table, alpha, gamma, state, action, reward, terminated, next_state)
-            #training_error.append(temporal_difference)
+            training_error.append(temporal_difference)
 
             done = terminated or truncated
             state = next_state
